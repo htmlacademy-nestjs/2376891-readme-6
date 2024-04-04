b4350480454030ad6bd9698e65c984a8f577336e
Snezhana Sterkhova <snezhana.sterkhova2015@gmail.com>
2024-04-04 14:10:57 +0300

    Adds the StorableEntity interface, implementation of BlogUserEntity and BlogUserFactory.

--- project/libs/shared/core/src/index.ts
+++ project/libs/shared/core/src/index.ts
@@ -1,3 +1,6 @@
 export { Entity } from './lib/base/entity';
 export { IUser } from './lib/types/user.interface.js';
-export {IAuthUser} from './lib/types/auth-user.interface.js';
+export { IAuthUser } from './lib/types/auth-user.interface.js';
+
+export { IStorableEntity } from './lib/interfaces/storable-entity.interface';
+export { IEntityFactory } from './lib/interfaces/entity-factory.interface';
