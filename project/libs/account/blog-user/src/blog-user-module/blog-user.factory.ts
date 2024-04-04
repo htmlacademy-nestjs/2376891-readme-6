25ded69abc2760019a9ed5787c82959d421f11ab
Snezhana Sterkhova <snezhana.sterkhova2015@gmail.com>
2024-04-04 16:06:58 +0300

    Adds the data-access library and BaseMemoryRepository, and implements BlogUserRepository.

--- project/libs/account/blog-user/src/blog-user-module/blog-user.factory.ts
+++ project/libs/account/blog-user/src/blog-user-module/blog-user.factory.ts
@@ -1,6 +1,9 @@
+import { Injectable } from '@nestjs/common';
+
 import { IAuthUser, IEntityFactory } from '@project/core';
 import { BlogUserEntity } from './blog-user.entity';
 
+@Injectable()
 export class BlogUserFactory implements IEntityFactory<BlogUserEntity> {
   public create(entityPlainData: IAuthUser): BlogUserEntity {
     return new BlogUserEntity(entityPlainData);
