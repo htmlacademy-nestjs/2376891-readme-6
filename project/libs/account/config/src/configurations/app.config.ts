8b2fcdf915bcf94cf06493bca8cb7cdde2cd38bd
Snezhana Sterkhova <snezhana.sterkhova2015@gmail.com>
2024-04-22 18:17:48 +0300

    Adds configuration for the accounts service and data validation from environment variables.

---
+++ project/libs/account/config/src/configurations/app.config.ts
@@ -0,0 +1,37 @@
+import { registerAs } from '@nestjs/config';
+import * as Joi from 'joi';
+
+const DEFAULT_PORT = 3000;
+const ENVIRONMENTS = ['development', 'production', 'stage'] as const;
+
+type TEnvironment = typeof ENVIRONMENTS[number];
+
+export interface IApplicationConfig {
+  environment: string;
+  port: number;
+}
+
+const validationSchema = Joi.object({
+  environment: Joi.string().valid(...ENVIRONMENTS).required(),
+  port: Joi.number().port().default(DEFAULT_PORT),
+});
+
+function validateConfig(config: IApplicationConfig): void {
+  const { error } = validationSchema.validate(config, { abortEarly: true });
+  if (error) {
+    throw new Error(`[Application Config Validation Error]: ${error.message}`);
+  }
+}
+
+function getConfig(): IApplicationConfig {
+  const config: IApplicationConfig = {
+    environment: process.env.NODE_ENV as TEnvironment,
+    port: parseInt(process.env.PORT || `${DEFAULT_PORT}`, 10),
+  };
+  console.log(config);
+
+  validateConfig(config);
+  return config;
+}
+
+export default registerAs('application', getConfig);
